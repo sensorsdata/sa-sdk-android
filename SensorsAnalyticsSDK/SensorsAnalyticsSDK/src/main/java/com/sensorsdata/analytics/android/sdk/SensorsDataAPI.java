0516b7541665e6cac9d479657ab08d6df760eee1
wangzhuozhou <wangzhuozhou@sensorsdata.cn>
2017-07-13 07:53:58 +0800

    Release 1.7.12

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/src/main/java/com/sensorsdata/analytics/android/sdk/SensorsDataAPI.java
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/src/main/java/com/sensorsdata/analytics/android/sdk/SensorsDataAPI.java
@@ -2055,7 +2055,7 @@ public class SensorsDataAPI {
     static final int VTRACK_SUPPORTED_MIN_API = 16;
 
     // SDK版本
-    static final String VERSION = "1.7.11";
+    static final String VERSION = "1.7.12";
 
     static Boolean ENABLE_LOG = false;
     static Boolean SHOW_DEBUG_INFO_VIEW = true;
