e2eee3e227a171ae2fee4df99ae1148ac5eef340
wangzhzh <wangzhuozhou@sensorsdata.cn>
2017-08-23 21:47:16 +0800

    Release 1.8.1

---
+++ SensorsAnalyticsSDK/src/main/java/com/sensorsdata/analytics/android/sdk/SensorsDataThreadPool.java
@@ -0,0 +1,35 @@
+package com.sensorsdata.analytics.android.sdk;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by 王灼洲 on 2016/12/28
+ */
+
+public class SensorsDataThreadPool {
+    private static SensorsDataThreadPool singleton;
+    private static Executor mExecutor;
+
+    public static SensorsDataThreadPool getInstance() {
+        if (singleton == null) {
+            synchronized (SensorsDataThreadPool.class) {
+                if (singleton == null) {
+                    singleton = new SensorsDataThreadPool();
+                    mExecutor = Executors.newFixedThreadPool(5);
+                }
+            }
+        }
+        return singleton;
+    }
+
+    public void execute(Runnable runnable) {
+        try {
+            if (runnable != null) {
+                mExecutor.execute(runnable);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
