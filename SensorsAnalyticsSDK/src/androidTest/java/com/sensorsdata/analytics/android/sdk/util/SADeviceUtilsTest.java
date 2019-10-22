e437905c47aba97fde273cb7c6efade7c0f8d9ca
dengshiwei <dengshiwei@sensorsdata.cn>
2019-10-22 10:15:09 +0800

    Release 3.2.7-pre

---
+++ SensorsAnalyticsSDK/src/androidTest/java/com/sensorsdata/analytics/android/sdk/util/SADeviceUtilsTest.java
@@ -0,0 +1,41 @@
+package com.sensorsdata.analytics.android.sdk.util;
+
+import android.Manifest;
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.GrantPermissionRule;
+
+import com.sensorsdata.analytics.android.sdk.SAConfigOptions;
+import com.sensorsdata.analytics.android.sdk.SensorsDataAPI;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNull;
+
+@RunWith(AndroidJUnit4.class)
+public class SADeviceUtilsTest {
+    @Rule
+    public GrantPermissionRule grantPermissionRule = GrantPermissionRule.grant(Manifest.permission.READ_PHONE_STATE);
+
+    @Before
+    public void initSensorsDataAPI(){
+        Context context = ApplicationProvider.getApplicationContext();
+        SensorsDataAPI.sharedInstance(context, new SAConfigOptions("").enableLog(true));
+    }
+
+    @Test
+    public void getOAID() {
+        try {
+            String oaid = SADeviceUtils.getOAID(ApplicationProvider.getApplicationContext());
+            assertNull(oaid);
+            SensorsDataAPI.sharedInstance().trackInstallation("AppInstall");
+        } catch (Exception ex) {
+            //ignore
+        }
+    }
+}
